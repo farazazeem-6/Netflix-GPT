cdce2f697d1e425771559bb2c0077f48b50b6c64
faraz-dhillon <faraz.tacticminds@gmail.com>
2025-11-11 15:31:25 +0500

    made just components files

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Netflix_Logo from "../../public/Netflix_Logo.png";
+const Header = () => {
+  return (
+    <div className="absolute z-30">
+      <img className="w-48" src={Netflix_Logo} alt="Netflix_Logo" />
+    </div>
+  );
+};
+
+export default Header;
+    <img
+          className=""
+          src=""
+          alt="Hero_Img"
+        />
